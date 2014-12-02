e7a67c2a734dd6d28f76c1a7ef4e1b1f2be8abdb
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-02 09:36:41 -0600

    First commit

---
+++ consoleapplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapplication1
+{
+    class Program
+    {
+        static void SampMethod(ref int i)
+        {
+            i = i * 8;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 9;
+            SampMethod(ref x);
+            Console.WriteLine("The value of x is {0}.", x);
+            Console.ReadKey();
+        }
+    }
+}
