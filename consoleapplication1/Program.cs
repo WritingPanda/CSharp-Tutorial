f0c53b9b02d44d0c9cf580594c791691a9bcf535
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-02 11:53:20 -0600

    Added program with the OUT keyword.
    Using out parameters to return multiple values from functions
    The out keyword causes arguments to be passed by reference.
    This is like the ref keyword, except that ref requires that the variable
    be initialized BEFORE it is passed.
    To use an out parameter, both the method definition and the calling
    method must explicitly use the out keyword.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -6,23 +6,33 @@ using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
+    // Using out parameters to return multiple values from functions
+    // The out keyword causes arguments to be passed by reference.
+    // This is like the ref keyword, except that ref requires that the variable be intialized BEFORE it is passed.
+    // To use an out parameter, both the method definition and the calling method must explicitly use the out keyword.
+    class test
+    {
+        public int outSample(out string first, out string second, int x, int y)
+        {
+            first = "Now I know how to add.";
+            second = "Now I know how to add again!";
+            return x + y;
+        }
+    }
+
     class Program
     {
-
-        // Using the ref modifier in function calls
-        // Using ref as the keyword in a function header allows treating basic types as if they were reference types
-        // Changing the value in the one function means that change is visible in other functions immediately
-        static void SampMethod(ref int i)
-        {
-            i = i * 8;
-        }
-
         static void Main(string[] args)
         {
-            int x = 9;
-            SampMethod(ref x);
-            Console.WriteLine("The value of x is {0}.", x);
-            Console.ReadKey();
+            test testObj = new test();
+
+            string first;
+            string second;
+            Console.WriteLine("The sum of 1 and 2 is {0}.", testObj.outSample(out first, out second, 1, 2));
+            Console.WriteLine(first + " " + second);
+            Console.Read();
         }
     }
 }
+
+// You can use the out contextual keyword in two contexts, as a parameter modifier or in generic type parameter declarations in interfaces and delegates. 
