081686c1049bd5c6600ff32324ccb9e45f360f80
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-02 17:00:40 -0600

    Enumerations
    The enum keyword is used to declare an enumeration, a distinct type that
    consists of a set of named constants called the enumerator list.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -6,34 +6,41 @@ using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
-    // Params keyword in function headers
+    // Enumerations
+    // The enum keyword is used to declare an enumeration, a distinct type that consists of a set of named constants called the enumerator list.
+    // Usually, it is best to define an enum directly within a namespace so that all classes in the namespace can access it with equal convenience. 
+    // However, an enum can also be nested within a class or a struct.
+    // By default, the first enumerator has the value of 0, and the value of each successive enumerator is increased by 1.
+
+    // In this example, we are using Sunday to be the first day of the week, so we change its value to 1.
+
+    // Approved types for an enum are byte, sbyte, short, ushort, int, uint, long, and ulong.
+
+    enum Days
+    {
+        // An enumerator cannot contain white space in its name.
+        Sunday = 1, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    };
+
+    enum intMaxMin : int
+    {
+        max = int.MaxValue, min = int.MinValue
+    };
+
     class Program
     {
-        public static void paramExample(int c, params object[] x) // params must be the final parameter in the list
-        {
-            Console.WriteLine(c);
-            
-            for (int i = 0; i < x.Length; i++)
-            {
-                Console.WriteLine("The value stored at index {0} is {1}.", i, x[i]);
-            }
-        }
-
         static void Main(string[] args)
         {
-            Program.paramExample(4, 1);
-            Program.paramExample(1, "example", "A", 2.45644);
+            Console.WriteLine("Sunday is the {0}st day of the week.", (int)Days.Sunday);
+            // An explicit cast is necessary to convert from enum type to an integral type.
+            Console.WriteLine("The maximum value of the integer data type is {0:N0} and the minimum value is {1:N0}.", (int)intMaxMin.max, (int)intMaxMin.min);
 
             Console.Read();
         }
     }
 }
 
-// Output:
+// Output
 
-// 4
-// The value stored at index 0 is 1.
-// 1
-// The value stored at index 0 is example.
-// The value stored at index 1 is A.
-// The value stored at index 2 is 2.45644.
+// Sunday is the 1st day of the week.
+// The maximum value of the integer data type is 2,147,483,647 and the minimum value is -2,147,483,648.
