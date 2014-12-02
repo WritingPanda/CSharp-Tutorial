61c5c267f6d0dc84df56cfe6c8f9f5dce33e8243
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-02 14:38:59 -0600

    Multidimensional Arrays (Matrix)
    Use type[,] variable = new type[rows, columns] to declare a matrix in
    C#.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
-
-    // Using the length property of arrays and converting between data types
+    // Multidimensional Arrays (Matrix)
     class Program
     {
         static void Main(string[] args)
         {
-            double[] myArray = new double[5]; // declaring and reserving an array that stores doubles[5 of them] (numbers with decimals)
+            int[,] matrix = new int[2, 2]; // Declare and initialize a multidimensional array of size 2 by 2. This means 4 cells ( 0, 1 by 0, 1 )
 
-            for (int i = 0; i < myArray.Length; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++) // Controls row index, meaning moves vertically down the matrix.
             {
-                // Start from index zero in the array and loop until we are less than the length of the array
-                
-                myArray[i] = (double)i / (double)(i + 1); // This code converts the index variable to a double data type for mathematics.
-                Console.WriteLine("The value of {0} divided by {1} is {2}.", i, i + 1, myArray[i]);
+                for (int j = 0; j < matrix.GetLength(1); j++) // Controls column index, meaning moves horizontally across the matrix
+                {
+                    matrix[i, j] = 1 + j;
+                }
             }
-            Console.Read();
         }
     }
 }
+
+// What this looks like
+// [0, 0] [0, 1]
+// [1, 0] [1, 1]
