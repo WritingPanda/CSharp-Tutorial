c4f02236140ddedef08835a4460064750960e499
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-02 10:58:29 -0600

    Added comments about the ref keyword.
    Using ref as the keyword in a function header allows treating basic
    types as if they were reference types.
    Changing the value in the one function means that the change is visible
    in other functions immediately.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -8,6 +8,10 @@ namespace consoleapplication1
 {
     class Program
     {
+
+        // Using the ref modifier in function calls
+        // Using ref as the keyword in a function header allows treating basic types as if they were reference types
+        // Changing the value in the one function means that change is visible in other functions immediately
         static void SampMethod(ref int i)
         {
             i = i * 8;
