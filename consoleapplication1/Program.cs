3bcdc9307844e7468eb5b5ef154b0c314d8b3dc3
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-03 11:52:23 -0600

    Difference between var and dynamic
    var is implicitly stating a type, which will be determined by the
    compiler. Dynamic is a static type (except for objects) that functions a
    lot like an object. Dynamic typed variables are assumed to support any
    operation.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -6,30 +6,32 @@ using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
-    // Dynamic Data Typing
+    // Difference between dynamic and var
     
     class Program
     {
         static void Main(string[] args)
         {
-            // The dynamic keyword represents an object whose operations will be resolved at runtime
-            dynamic x;
+            // Variables that are declared at method scope can have an IMPLICIT type var.
+            // An implicitly typed local variable is strongly typed just as if you had declared the type yourself.
+            // The compiler determines the type.
+            // var i = 10; IMPLICITLY typed
+            // int i = 10; EXPLICITLY typed
+            var x = 5;
+            var y = "Hello";
 
-            x = 5;
-            // x becomes an int
-            Console.WriteLine(x);
+            // The type is a static type, but an object of type dynamic bypasses static type checking. 
+            // In most cases, it functions like it has type object. 
+            // At compile time, an element that is typed as dynamic is assumed to support any operation. 
+            // Therefore, you do not have to be concerned about whether the object gets its value from a COM API, from a dynamic language such as IronPython, from the HTML Document Object Model (DOM), from reflection, or from somewhere else in the program. 
+            // However, if the code is not valid, errors are caught at run time.
+            dynamic z = x;
+            Console.WriteLine(z);
 
-            x = "hello";
-            // x becomes a string
-            Console.WriteLine(x);
-
-            x = 'C';
-            // x becomes a char
-            Console.WriteLine(x);
+            z = y;
+            Console.WriteLine(z);
 
             Console.Read();
-
-            // Local variable x is a dynamic {int}, then a dynamic {string}, and then a dynamic {char}.
         }
     }
 }
