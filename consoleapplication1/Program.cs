cda81ae54c897da2980ef9c1fb14444960e7f2b1
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-12-09 13:53:44 -0600

    Setting up a simple inheritance example
    Classes can inherit properties and methods from other classes.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -6,19 +6,32 @@ using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
-    // Class level fields sharing between among objects
+    // Setting up a simple inheritance example
+
+    class quad
+    {
+        public quad() // this will be our parent class
+        {
+            Console.WriteLine("I've got four sides!");
+        }
+    }
+
+    class rectangle : quad // inheriting from the quad class
+    {
+        // parent quad class' constructor is used since rectangle does not have a constructor of its own
+        public double Area( double x, double y )
+        {
+            return x * y;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Class1 myObj1 = new Class1(7);
-            Class1 myObj2 = new Class1(8);
-
-            Console.WriteLine("This instance variable is {0}.", myObj1.GetY());
-            Console.WriteLine("This instance variable is {0}.", myObj2.GetY());
-            Console.WriteLine("The value of the static instance variable is {0}.", Class1.GetValue());
-
+            rectangle myRect = new rectangle();
+            double l = 12; double w = 13;
+            Console.WriteLine("This area of our rectangle is {0}.", myRect.Area(l, w));
             Console.Read();
         }
     }
