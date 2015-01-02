50dd566a0a8b57b02b7199cc75d9efcc64b682a0
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-01 19:04:39 -0600

    Polymorphism
    You create a class of type Vehicle. You then create three subclasses
    called Sedan, Truck, and Minivan. Each of these can be added to a
    collection containing Vehicles. This is so because each is a kind of
    Vehicle, and therefore polymorphism applies.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace consoleapplication1
 {
-    // Equality by references
+    // Polymorphism
 
-    class Person
+    public class Shape
     {
-        public string firstName;
-        public string lastName;
-
-        public Person(string fname, string lname)
+        // Virtual method
+        public virtual void Draw()
         {
-            Console.WriteLine("My name is {0} {1}.", this.firstName = fname, this.lastName = lname);
+            Console.WriteLine("Performing base class drawing tasks.");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public Circle()
+        {
+            Console.WriteLine("I am a circle.");
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square()
+        {
+            Console.WriteLine("I am a square.");
+        }
+
+        public override void Draw()
+        {
+            // Code to draw a circle
+            Console.WriteLine("Drawing a square.");
         }
     }
 
@@ -23,25 +40,28 @@ namespace consoleapplication1
     {
         static void Main(string[] args)
         {
-            Person pr1 = new Person("Omar", "Quimbaya");
-            Console.WriteLine("The person's name is {0} {1}.", pr1.firstName, pr1.lastName);
+            // Polymorphism at work #1: a Rectangle, Triangle, and Circle
+            // can all be used wherever a Shape is expected. No cast is
+            // required because an implicit conversion exists from a derived
+            // class to its base class. Each circle and square object is
+            // treated as a shape, also. So it has two forms, polymorphism in action.
+            List<Shape> newShapes = new List<Shape>();
+            newShapes.Add(new Circle()); // expects the Shape object but gets a circle
+            newShapes.Add(new Square()); // expects the Shape object but gets a square
 
-            // Points to the same reference in memory, so if one is changed, they are both changed.
-            Person pr2 = pr1;
-            
-            Console.WriteLine("It is {0} that pr1 and pr2 are the same.", pr1 == pr2);
+            // Polymorphism at work #2: the virtual method Draw() is
+            // invoked on each of the derived classes, not the base class.
 
-            // This changes both pr1 and pr2 since they both point to the same place in memory.
-            pr2.firstName = "Becky";
-
-            // This is true.
-            Console.WriteLine("It is {0} that pr1 and pr2 are the same.", pr1 == pr2);
-
-            // Both of these say "Becky Quimbaya"
-            Console.WriteLine("The person's name is {0} {1}.", pr1.firstName, pr1.lastName);
-            Console.WriteLine("The person's name is {0} {1}.", pr2.firstName, pr2.lastName);
-            
+            foreach (Shape shape in newShapes)
+            {
+                shape.Draw(); // runtime polymorphism. Expects to call base class draw but invokes derived class method
+            }
+            Console.WriteLine("Press any key to exit.");
             Console.Read();
+
+            // You create a class of type Vehicle. You then create three subclasses called Sedan, Truck, and Minivan. 
+            // Each of these can be added to a collection containing Vehicles. This is so because each is a kind of Vehicle, 
+            // and therefore polymorphism applies.
         }
     }
 }
