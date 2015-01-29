603dbf68ed71a2555d29cd38c7ea4ca2ad49d0f1
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-29 13:34:09 -0600

    Delegates
    A delegate is a type that represents references to methods with a
    particular parameter list and return type. When you instantiate a
    delegate, you can associate its instance with any method with a
    compatible signature and return type. You can invoke (or call) the
    method through the delegate instance.
    Delegates are used to pass methods as arguments to other methods. Event
    handlers are nothing more than methods that are invoked through
    delegates. You create a custom method, and a class such as a windows
    control can call your method when a certain event occurs.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -1,56 +1,47 @@
 ﻿
 using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32;
 
 namespace consoleapplication1
 {
-    // Overloading Operators 2
-    
+    // Delegates
+    /// <summary>
+    /// A delegate is a type that represents references to methods with a 
+    /// particular parameter list and return type. When you instantiate a 
+    /// delegate, you can associate its instance with any method with a 
+    /// compatible signature and return type. You can invoke (or call) 
+    /// the method through the delegate instance. Delegates are used to 
+    /// pass methods as arguments to other methods. Event handlers are 
+    /// nothing more than methods that are invoked through delegates. 
+    /// You create a custom method, and a class such as a windows 
+    /// control can call your method when a certain event occurs. 
+    /// </summary>
+
+    internal delegate void D(int x);
+
     class Program
     {
-        class PerimFinder
+        public void SampFunc1(int i)
         {
-            private int lengthOfSide;
-            private int numberOfSides;
-
-            public PerimFinder(int length, int n)
-            {
-                this.lengthOfSide = length;
-                this.numberOfSides = n;
-            }
-
-            public static int operator +(PerimFinder s1, PerimFinder s2)
-            {
-                return s1.numberOfSides*s1.lengthOfSide + s2.lengthOfSide*s2.numberOfSides;
-            }
+            Console.WriteLine("{0} squared is {1}", i, i * i);
         }
 
+        public void SampFunc2(int i)
+        {
+            Console.WriteLine("{0} cubed is {1}", i, i*i*i);
+        }
         static void Main(string[] args)
         {
-            ConsoleKeyInfo conkey;
-            int x;
-            int y;
-            int xNumberOfSides;
-            int yNumberOfSides;
+            Program newprog = new Program();
 
-            do
-            {
-                Console.WriteLine("Enter the first side length: ");
-                x = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the number of sides for this shape: ");
-                xNumberOfSides = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter the first side length: ");
-                y = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the number of sides for this shape: ");
-                yNumberOfSides = int.Parse(Console.ReadLine());
-
-                PerimFinder newPerim1 = new PerimFinder(x, xNumberOfSides);
-                PerimFinder newPerim2 = new PerimFinder(y, yNumberOfSides);
-                Console.WriteLine(newPerim1 + newPerim2);
-
-                // Console Key, a way to capture key input and do something with it
-                conkey = Console.ReadKey();
-            } while (conkey.Key != ConsoleKey.Escape);
+            D newDel1, newDel2, newDel3;
+            newDel1 = newprog.SampFunc1;
+            newDel2 = newprog.SampFunc2;
+            // Calls both functions
+            newDel3 = newDel1 + newDel2;
+            newDel3(4);
+            Console.Read();
         }
     }
 }
