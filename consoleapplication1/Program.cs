9a6abe23da4d5caeca50d21141c31b1d111121de
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-29 13:50:38 -0600

    Multicast Delegates
    Method chaining delegates

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -5,42 +5,32 @@ using Microsoft.Win32;
 
 namespace consoleapplication1
 {
-    // Delegates
-    /// <summary>
-    /// A delegate is a type that represents references to methods with a 
-    /// particular parameter list and return type. When you instantiate a 
-    /// delegate, you can associate its instance with any method with a 
-    /// compatible signature and return type. You can invoke (or call) 
-    /// the method through the delegate instance. Delegates are used to 
-    /// pass methods as arguments to other methods. Event handlers are 
-    /// nothing more than methods that are invoked through delegates. 
-    /// You create a custom method, and a class such as a windows 
-    /// control can call your method when a certain event occurs. 
-    /// </summary>
+    // Multi-cast Delegates
 
-    internal delegate void D(int x);
+    // A delegate is a method wrapper
+    delegate void D(int x);
 
     class Program
     {
-        public void SampFunc1(int i)
+        public void Square(int i)
         {
-            Console.WriteLine("{0} squared is {1}", i, i * i);
+            Console.WriteLine("{0} squared is {1}", i, i*i);
         }
 
-        public void SampFunc2(int i)
+        public void Cube(int i)
         {
-            Console.WriteLine("{0} cubed is {1}", i, i*i*i);
+            Console.WriteLine("{0} cubed is {1}", i, i * i * i);
         }
+
         static void Main(string[] args)
         {
             Program newprog = new Program();
+            D del1, del2, del3;
+            del1 = newprog.Square;
+            del2 = newprog.Cube;
+            del3 = del1 + del2;
+            del3(4);
 
-            D newDel1, newDel2, newDel3;
-            newDel1 = newprog.SampFunc1;
-            newDel2 = newprog.SampFunc2;
-            // Calls both functions
-            newDel3 = newDel1 + newDel2;
-            newDel3(4);
             Console.Read();
         }
     }
