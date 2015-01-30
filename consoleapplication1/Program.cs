9ed9a530061606df64b9ee99ae300a2c57f0a85b
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-30 16:03:25 -0600

    Program that waits for user input

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -3,16 +3,34 @@
 namespace consoleapplication1
 {
     ///<summary>
-    /// Partial Class
+    /// Create a program that contiually
+    /// waits for user input
     ///</summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            Employee newEmployee = new Employee("Omar");
-            Console.WriteLine("The name of the person is {0}.", newEmployee.GetFirst());
-            Console.Read();
+            Console.WriteLine("Choose color: (B)lue or (R)ed.");
+            string color = Console.ReadLine();
+            string upperColor = color.ToUpper();
+            if (upperColor == "B")
+            {
+                Console.ForegroundColor = System.ConsoleColor.Cyan;
+            }
+            if (upperColor == "R")
+            {
+                Console.ForegroundColor = System.ConsoleColor.Red;
+            }
+            Console.WriteLine("Enter value whose absolute value you wish to find.");
+            Console.WriteLine("Enter 0 to exit");
+            double x = double.Parse(Console.ReadLine());
+            while (x != 0)
+            {
+                Console.WriteLine("The absolute value of your number is {0}.", Math.Abs(x));
+                Console.WriteLine("Enter value whose absolute value you wish to find.");
+                x = double.Parse(Console.ReadLine());
+            }
         }
     }
 }
