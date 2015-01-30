bbfc80f1c0fe46834402ba6cecdd851b8262ace6
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-30 16:37:20 -0600

    Basics of checking for errors

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -3,34 +3,34 @@
 namespace consoleapplication1
 {
     ///<summary>
-    /// Create a program that contiually
-    /// waits for user input
+    /// Error Checking with if/else
     ///</summary>
+
+    class Calculator
+    {
+        public Calculator()
+        {
+            Console.WriteLine("I'm a lazy calculator. I do only division.");
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y != 0) return x / y;
+
+            Console.WriteLine("Whoa! WTF! You can't divide by 0!");
+            return 0.0;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose color: (B)lue or (R)ed.");
-            string color = Console.ReadLine();
-            string upperColor = color.ToUpper();
-            if (upperColor == "B")
-            {
-                Console.ForegroundColor = System.ConsoleColor.Cyan;
-            }
-            if (upperColor == "R")
-            {
-                Console.ForegroundColor = System.ConsoleColor.Red;
-            }
-            Console.WriteLine("Enter value whose absolute value you wish to find.");
-            Console.WriteLine("Enter 0 to exit");
-            double x = double.Parse(Console.ReadLine());
-            while (x != 0)
-            {
-                Console.WriteLine("The absolute value of your number is {0}.", Math.Abs(x));
-                Console.WriteLine("Enter value whose absolute value you wish to find.");
-                x = double.Parse(Console.ReadLine());
-            }
+            Calculator calc = new Calculator();
+            int x = 4;
+            int y = 0;
+            Console.WriteLine("The result of your division is: {0}.", calc.Divide(x, y));
+            Console.Read();
         }
     }
 }
