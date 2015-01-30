68bfc01360d1b674c527c61ad5e467df51b0476d
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-01-30 12:52:45 -0600

    Access modifiers

--- consoleapplication1/Area.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace consoleapplication1
-{
-    // Can change implementation of the interface without
-    // breaking the entire program
-    interface Area
-    {
-        double Area(double width, double height);
-    }
-}
---
+++ consoleapplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace consoleapplication1
+{
+    ///<summary>
+    /// Access Modifiers
+    ///</summary>
+
+    class Parent
+    {
+        public static int x = 10;
+        protected static int y = 12;
+        private static int z = 25;
+
+        public static void Print()
+        {
+            Console.WriteLine(z);
+        }
+    }
+
+    class Child : Parent
+    {
+        public static void Print()
+        {
+            Console.WriteLine(y);
+        }
+    }
+
+    class NotConnected
+    {
+        public static void Print()
+        {
+            Console.WriteLine(Parent.x);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent.Print();
+            Child.Print();
+            NotConnected.Print();
+            Console.Read();
+        }
+    }
+}
