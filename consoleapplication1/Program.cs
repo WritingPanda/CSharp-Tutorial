3f676bfd8a126950c7537e5741ddabaee9a83e64
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-05 10:18:09 -0600

    Git error

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -3,30 +3,14 @@
 namespace consoleapplication1
 {
     ///<summary>
-    /// Try Catch Finally Construct
+    /// 
     ///</summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 12;
-            object o = "hello";
-
-            try
-            {
-                i = (int) o; // tries to convert an object into an integer
-            }
-            catch (InvalidCastException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("This code runs no matter what happens above.");
-            }
-
-            Console.Read();
+            
         }
     }
 }
