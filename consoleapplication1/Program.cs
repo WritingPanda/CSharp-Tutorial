a02bb52de490c8fcbe87d7036529270530a2b80b
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-06 17:03:58 -0600

    Writing and Reading Files
    System.IO
    TextWriter
    TextReader
    
    Make sure you close to avoid memory leaks.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -1,40 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace consoleapplication1
 {
     ///<summary>
-    /// Objects into Arrays with Indexers
-    /// Indexer takes an object and turns it into an array
+    /// Writing and reading files
     ///</summary>
-
-    class IndexerExample
-    {
-        private readonly float[] _myarray = new float[3] { 3.4F, 5.6F, 8.9F };
-
-        // header of indexer. Returns a float and accepts an integer
-        // the float is the value from the array above, and the integer is the index in the array
-        public float this[int i]
-        {
-            // returns a value from the array above
-            get { return _myarray[i]; }
-            // sets a value inside the array
-            set { _myarray[i] = value; }
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            IndexerExample myExample = new IndexerExample();
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("The index {0} stores the value: {1}", i, myExample[i]);
-            }
+            const string filename = "C:\\Users\\Omar\\Desktop\\sample.txt";
+            TextWriter tw = new StreamWriter(filename);
+            // Will read from the console window and save to the file
+            tw.Write(Console.ReadLine());
+            tw.Close();
 
-            Console.WriteLine("The first index stores the value: {0}", myExample[0]);
-            Console.WriteLine("The second index stores the value: {0}", myExample[1]);
-            Console.WriteLine("The third index stores the value: {0}", myExample[2]);
+            TextReader tr = new StreamReader(filename);
+            // Will read from the file and display to the output window
+            Console.WriteLine(tr.ReadLine());
+            tr.Close();
+
             Console.Read();
         }
     }
