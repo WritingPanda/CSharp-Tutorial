496aae56e9d0a403f5c03c2098cc7abee41c8451
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-06 13:56:07 -0600

    The Get Type Method
    Gets the type of an object

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -1,14 +1,38 @@
-﻿namespace consoleapplication1
+﻿using System;
+
+namespace consoleapplication1
 {
     ///<summary>
-    ///
+    /// Get Type Method
+    /// Gets the type of an object
     ///</summary>
+
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Class 1 object.");
+        }
+    }
+
+    public class Class2
+    {
+        public Class2()
+        {
+            Console.WriteLine("Class 2 object.");
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Class1 klass1 = new Class1();
+            Class2 klass2 = new Class2();
+            Console.WriteLine(klass1.GetType());
+            Console.WriteLine(klass2.GetType());
+            Console.WriteLine(klass1.GetType() == klass2.GetType());
+            Console.Read();
         }
     }
 }
