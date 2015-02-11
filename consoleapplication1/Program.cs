74cbb5bd827fd2adee5c01ef57e8698a85014629
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-11 15:45:46 -0600

    Generics
    Generics let you tailor a method, class, structure, or interface to the
    precise data type it acts upon. For example, instead of using the
    Hashtable class, which allows keys and values to be of any type, you can
    use the Dictionary<TKey, TValue> generic class and specify the type
    allowed for the key and the type allowed for the value. Among the
    benefits of generics are increased code reusability and type safety.

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -1,32 +1,27 @@
 ﻿using System;
-using System.IO;
 
 namespace consoleapplication1
 {
     /// <summary>
-    /// Display files and folders in a directory
+    /// Intro to Generics
+    /// Generics let you tailor a method, class, structure, 
+    /// or interface to the precise data type it acts upon.
+    /// Among the benefits of generics are increased code 
+    /// reusability and type safety.
     /// </summary>
-
-    
 
     class Program
     {
         static void Main(string[] args)
         {
-            // Use @ in front of a string to avoid using escape characters
-            string[] files = Directory.GetFiles(@"C:\Users\Omar\Documents\data");
-            string[] folders = Directory.GetDirectories(@"C:\Users\Omar\Documents");
+            // 
+            Generics<int> myGenerics1 = new Generics<int>(5);
+            int y1 = myGenerics1.Get();
+            Console.WriteLine("The value of y1 is {0}.", y1);
 
-            foreach (string f in files)
-            {
-                Console.WriteLine("The file called {0} is located in {1}.", Path.GetFileName(f), Path.GetDirectoryName(f));
-            }
-
-            foreach (string f in folders)
-            {
-                Console.WriteLine("The directory is {0}.", f.ToString());
-            }
-
+            Generics<string> myGenerics2 = new Generics<string>("Hello world");
+            string y2 = myGenerics2.Get();
+            Console.WriteLine("The value of y2 is {0}.", y2);
             Console.Read();
         }
     }
