c15f1638a1d0360660b14a7f644439081babc733
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-12 15:21:15 -0600

    Lambda Expressions with Outer Variables

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -3,21 +3,26 @@
 namespace consoleapplication1
 {
     /// <summary>
-    /// Func<> Notation for Lambda Expressions
+    /// Using Lambda Expressions with Outer Variables
+    /// A lambda expression is an anonymous function 
+    /// that you can use to create delegates or expression tree types.
+    /// By using lambda expressions, you can write local functions 
+    /// that can be passed as arguments or returned as the value of 
+    /// function calls. Lambda expressions are particularly helpful 
+    /// for writing LINQ query expressions.
     /// </summary>
 
     class Program
     {
         static void Main(string[] args)
         {
-            // Declare code of a type function
-            Func<String> myFunc1 = () => "Hello world";
-            Console.WriteLine("Func1 returns {0}.", myFunc1());
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
 
-            // Three ints for two parameters and one return value
-            Func<int, int, int> myFunc2 = (int a, int b) => (a + b);
-            Console.WriteLine("The sum of 4 and 10 is {0}.", myFunc2(4, 10));
-
+            Func<int, int, int> myFunc1 = (int a, int b) => (a + b);
+            Func<int, int, int> myFunc2 = (int a, int b) => (a - b);
+            Console.WriteLine("The sum of {0} and {1} is {2}.", x, y, myFunc1(x, y));
+            Console.WriteLine("The difference of {0} and {1} is {2}.", x, y, myFunc2(x, y));
             Console.Read();
         }
     }
