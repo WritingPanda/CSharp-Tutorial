7c18c2ff8b539c94bde244bea886f56ed99ac165
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-16 18:06:32 -0600

    Overloading Operators

--- consoleapplication1/Program.cs
+++ consoleapplication1/Program.cs
@@ -4,25 +4,40 @@ using System.IO;
 namespace consoleapplication1
 {
     /// <summary>
-    /// Using the Using Statement
+    /// Overloading Operators
     /// </summary>
+
+    public class TwoDVector
+    {
+        public double xcomp;
+        public double ycomp;
+
+        public TwoDVector(double x, double y)
+        {
+            this.xcomp = x;
+            this.ycomp = y;
+        }
+
+        // Overloading an operator, or giving it new operations so that it performs in a new way
+        public static double operator * (TwoDVector first, TwoDVector second)
+        {
+            return first.xcomp * second.xcomp + first.ycomp * second.ycomp;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            using (TextWriter writer = File.CreateText("log.txt"))
-            {
-                writer.WriteLine(Console.ReadLine());
-            }
+            double xcomp1 = Double.Parse(Console.ReadLine());
+            double ycomp1 = Double.Parse(Console.ReadLine());
+            TwoDVector vec1 = new TwoDVector(xcomp1, ycomp1);
 
-            using (TextReader reader = File.OpenText("log.txt"))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-            }
+            double xcomp2 = Double.Parse(Console.ReadLine());
+            double ycomp2 = Double.Parse(Console.ReadLine());
+            TwoDVector vec2 = new TwoDVector(xcomp2, ycomp2);
+
+            Console.Write("The total price of your purchases is {0}.", vec1*vec2);
 
             Console.Read();
         }
